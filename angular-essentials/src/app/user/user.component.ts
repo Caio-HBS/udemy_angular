ae3baf8ce2e46105a6946c2131a12507b7f95837
Caio Bianchi <caiohbs23@gmail.com>
2024-08-07 12:05:47 -0300

    Progress on components

---
+++ angular-essentials/src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { DUMMY_USERS } from "../dummy-users";
+
+const randIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randIndex];
+
+  get imagePath() {
+    return "assets/users/" + this.selectedUser.avatar
+  }
+}
