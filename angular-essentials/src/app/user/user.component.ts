36f544ab3bc8b0f8c308368c83e56ff9406ee611
Caio Bianchi <caiohbs23@gmail.com>
2024-08-07 16:32:38 -0300

    Added customizable info for component

--- angular-essentials/src/app/user/user.component.ts
+++ angular-essentials/src/app/user/user.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
-
-import { DUMMY_USERS } from "../dummy-users";
-
-const randIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randIndex];
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+  @Output() selectUser = new EventEmitter<string>();
 
   get imagePath() {
-    return "assets/users/" + this.selectedUser.avatar
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.selectUser.emit(this.id);
   }
 }
