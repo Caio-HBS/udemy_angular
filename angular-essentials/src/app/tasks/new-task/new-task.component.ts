6d7cff30cb652932e71df54cb5c76fb73bc27c1c
Caio Bianchi <caiohbs23@gmail.com>
2024-08-12 16:19:37 -0300

    implemented services

--- angular-essentials/src/app/tasks/new-task/new-task.component.ts
+++ angular-essentials/src/app/tasks/new-task/new-task.component.ts
@@ -1,7 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { type NewTask } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,22 +11,28 @@ import { type NewTask } from '../task/task.model';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() addEvent = new EventEmitter<NewTask>();
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+
+  private tasksService = inject(TasksService);
 
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onFormSubmit() {
-    this.addEvent.emit({
-      title: this.enteredTitle,
-      summary: this.enteredSummary,
-      date: this.enteredDate,
-    });
+    this.tasksService.addTask(
+      {
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        date: this.enteredDate,
+      },
+      this.userId
+    );
+    this.close.emit();
   }
 }
