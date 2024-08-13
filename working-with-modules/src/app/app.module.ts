21990dfc8e0dd383340b03df641ea8492ba29a19
Caio Bianchi <caiohbs23@gmail.com>
2024-08-13 11:27:26 -0300

    added first modules

---
+++ working-with-modules/src/app/app.module.ts
@@ -0,0 +1,6 @@
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({ declarations: [AppComponent], bootstrap: [AppComponent] })
+export class AppModule {}
