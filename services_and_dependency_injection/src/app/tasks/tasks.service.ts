d5c5bc206881dc59ec6954beb157d298bfc0228d
Caio Bianchi <caiohbs23@gmail.com>
2024-08-30 15:54:46 -0300

    added and implemented new service

--- services_and_dependency_injection/src/app/tasks/tasks.service.ts
+++ services_and_dependency_injection/src/app/tasks/tasks.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, signal } from "@angular/core";
 
 import { Task, TaskStatus } from "./task.model";
-import { single } from "rxjs";
+
+import { LoggingService } from "../logging.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class TasksService {
   private tasks = signal<Task[]>([]);
+
+  constructor(private loggingService: LoggingService) {}
 
   allTasks = this.tasks.asReadonly();
 
@@ -19,6 +22,7 @@ export class TasksService {
     };
 
     this.tasks.update((prevTasks) => [...prevTasks, newTask]);
+    this.loggingService.log(`ADDED TASK WITH TITLE: ${taskData.title}`);
   }
 
   updateTask(taskId: string, newStatus: TaskStatus) {
@@ -29,5 +33,6 @@ export class TasksService {
           : singleTask
       )
     );
+    this.loggingService.log(`CHANGED TASK STATUS: ${newStatus}`);
   }
 }
