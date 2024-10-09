5f9b0de683dd3531ec81dbbbb1950f96235ef21e
Caio Bianchi <caiohbs23@gmail.com>
2024-10-09 10:13:00 -0300

    implemented a service responsible for fetching data

--- http_requests/src/app/places/places.service.ts
+++ http_requests/src/app/places/places.service.ts
@@ -1,4 +1,6 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { catchError, throwError } from "rxjs";
 
 import { Place } from "./place.model";
 
@@ -6,15 +8,41 @@ import { Place } from "./place.model";
   providedIn: "root",
 })
 export class PlacesService {
+  private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
 
   loadedUserPlaces = this.userPlaces.asReadonly();
 
-  loadAvailablePlaces() {}
+  loadAvailablePlaces() {
+    return this.fetchPlaces(
+      "http://localhost:3000/places",
+      "Oops! Something went wrong, try again later."
+    );
+  }
 
-  loadUserPlaces() {}
+  loadUserPlaces() {
+    return this.fetchPlaces(
+      "http://localhost:3000/user-places",
+      "Oops! Something went wrong fetching your places, try again later."
+    );
+  }
 
-  addPlaceToUserPlaces(place: Place) {}
+  addPlaceToUserPlaces(placeId: string) {
+    return this.httpClient.put("http://localhost:3000/user-places", {
+      placeId,
+    });
+  }
 
   removeUserPlace(place: Place) {}
+
+  private fetchPlaces(url: string, errMessage: string) {
+    return this.httpClient.get<{ places: Place[] }>(url).pipe(
+      catchError((err: Error) =>
+        throwError(() => {
+          console.log(err);
+          return new Error(errMessage);
+        })
+      )
+    );
+  }
 }
