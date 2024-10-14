17d671ff5a9a319c42d0e011978e83ae0335547a
Caio Bianchi <caiohbs23@gmail.com>
2024-10-14 10:50:47 -0300

    fixed new-task component

--- routing/src/app/tasks/new-task/new-task.component.ts
+++ routing/src/app/tasks/new-task/new-task.component.ts
@@ -2,20 +2,24 @@ import { Component, inject, input, signal } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { TasksService } from "../tasks.service";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-new-task",
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: "./new-task.component.html",
   styleUrl: "./new-task.component.css",
 })
 export class NewTaskComponent {
+  private tasksService = inject(TasksService);
+  private routerService = inject(Router);
+
   userId = input.required<string>();
+
   enteredTitle = signal("");
   enteredSummary = signal("");
   enteredDate = signal("");
-  private tasksService = inject(TasksService);
 
   onSubmit() {
     this.tasksService.addTask(
@@ -26,5 +30,8 @@ export class NewTaskComponent {
       },
       this.userId()
     );
+    this.routerService.navigate(["/users", this.userId(), "tasks"], {
+      replaceUrl: true,
+    });
   }
 }
