b8ab8c817db7a1e90316c0dc7f242a5d78944b4b
Caio Bianchi <caiohbs23@gmail.com>
2024-10-14 11:03:22 -0300

    added sort functionality

--- routing/src/app/tasks/tasks.component.ts
+++ routing/src/app/tasks/tasks.component.ts
@@ -3,20 +3,31 @@ import { Component, computed, inject, input } from "@angular/core";
 import { TasksService } from "./tasks.service";
 
 import { TaskComponent } from "./task/task.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-tasks",
   standalone: true,
   templateUrl: "./tasks.component.html",
   styleUrl: "./tasks.component.css",
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   private tasksService = inject(TasksService);
 
+  sort = input<"asc" | "desc">();
   userId = input.required<string>();
 
   userTasks = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.sort() === "desc") {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      })
   );
 }
